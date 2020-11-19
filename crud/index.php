ad201bcba7cd69a745d482ddfdf53e4913783a11
Thitiwut11 <73686921+Thitiwut85@users.noreply.github.com>
2020-11-19 20:01:44 +0700

    Update index.php

--- crud/index.php
+++ crud/index.php
@@ -30,7 +30,7 @@
 								}
 								$res = mysqli_query($conn, 'SELECT * FROM guestbook');
 							?>
-							<table class="table table-secondary table-hover">
+							<table class="table table-active table-hover">
 								<thead>
 									<tr>
 										<th>Name</th>
