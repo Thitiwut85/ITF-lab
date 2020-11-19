80faa9087bcdf76c31aebac728d0e629bf9d6897
Thitiwut11 <73686921+Thitiwut85@users.noreply.github.com>
2020-11-19 18:55:31 +0700

    Add files via upload

---
+++ crud/edit.php
@@ -0,0 +1,61 @@
+<?php
+	$conn = mysqli_connect('thitiwut.mysql.database.azure.com', 'thitiwut@thitiwut', 'Aom26rock', 'ITFLab');
+
+	$id = $_GET['ID'];
+
+	$sql = 'SELECT * FROM guestbook WHERE ID = '.$id.'';
+	$query = mysqli_query($conn, $sql);
+	if(!$query) {
+		header('Location: index.php');
+	}
+	else {
+		$data = mysqli_fetch_assoc($query);
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #3380FF;
+		}
+	</style>
+</head>
+<body class="bg-secondary py-5">
+	<div class="container p-3 my-3 bg-dark text-white">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-6"><h1 class="text-monospace">Edit</h1></div>
+						</div>
+						<form action="update.php" method="post">
+							<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
+							<div class="form-group">
+								<label>Name</label>
+								<input type="text" name="name" value="<?php echo $data['Name']; ?>" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Comment</label>
+								<textarea name="comment" class="form-control" rows="5" required><?php echo $data['Comment']; ?></textarea>
+							</div>
+							<div class="form-group">
+								<label>Link</label>
+								<input type="text" name="link" value="<?php echo $data['Link']; ?>" class="form-control">
+							</div>
+							<div class="text-center">
+								<button type="submit" class="btn btn-sm btn-success my-3 font-weight-bold">SAVE</button>
+								<a class="btn btn-sm btn-light my-3" href="index.php">CANCLE</a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ crud/update.php
@@ -0,0 +1,46 @@
+<?php
+	$conn = mysqli_connect('thitiwut.mysql.database.azure.com', 'thitiwut@thitiwut', 'Aom26rcok', 'ITFLab');
+
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$link = $_POST['link'];
+	$id = $_POST['id'];
+
+	$sql = 'UPDATE guestbook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #3380FF;
+		}
+	</style>
+</head>
+<body class="bg-secondary py-5">
+	<div class="container p-3 my-3 bg-dark text-white">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "EDIT SUCCESS";
+							}
+							else {
+								echo "FAILED";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-dark">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
