c14d757a577b678454e771d348ab3aece3bf29cd
Thitiwut11 <73686921+Thitiwut85@users.noreply.github.com>
2020-11-27 13:57:25 +0700

    Update testinsert.php

--- testinsert.php
+++ testinsert.php
@@ -13,7 +13,7 @@ $B = $_POST['B'];
 $C = $A+$B;
 
 
-$sql = "INSERT INTO guestbook (A , B , B) VALUES ('$A', '$B', '$C')";
+$sql = "INSERT INTO guestbook (A , B , C) VALUES ('$A', '$B', '$C')";
 
 
 if (mysqli_query($conn, $sql)) {
