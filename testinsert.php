4393943d7fbb672cb7e0f34c399c749941e6206e
Thitiwut11 <73686921+Thitiwut85@users.noreply.github.com>
2020-11-27 10:29:34 +0700

    Add files via upload

---
+++ testinsert.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'thitiwut.mysql.database.azure.com', 'thitiwut@thitiwut', 'Aom26rock', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$A = $_POST['A'];
+$B = $_POST['B'];
+$C = $A+$B;
+
+
+$sql = "INSERT INTO guestbook (A , B , B) VALUES ('$A', '$B', '$C')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Success";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
